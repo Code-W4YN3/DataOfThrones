b43cb21347ffc04cc29b715e1dc0938a83e570b3
Wayne Otido <otidowayne@gmail.com>
2023-07-05 23:09:05 +0300

    styled character info

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ function displayCharacters(){
     let listItem = document.createElement('button') 
     listItem.innerText = `${character.fullName}`
     list.appendChild(listItem)
+    let picture = document.getElementById('charImage')
+    let about = document.getElementById('charData')
     listItem.addEventListener('click', ()=>{
-      let picture = document.getElementById('charImage')
-      let about = document.getElementById('charData')
       picture.innerHTML= `
-        <img src="${character.imageUrl}">
+        <img id ="characterPicture" src="${character.imageUrl}">
       `
       about.innerHTML=`
-        <p>First Name: ${character.firstName}</p>
-        <p>Last Name: ${character.lastName}</p>
-        <p>Title: ${character.title}</p>
-        <p>Family: ${character.family}</p>
+        <p><b>First Name:</b> ${character.firstName}</p>
+        <p><b>Last Name:</b> ${character.lastName}</p>
+        <p><b>Title:</b> ${character.title}</p>
+        <p><b>Family:</b> ${character.family}</p>
       `
     })
   }))
 }
+
